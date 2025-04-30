d435f82dafa9857d54a57b65bbaed6f8c800cfe0
andrii <a.pyvovarov@steel.mil.ua>
2025-04-30 14:54:02 +0300

    feat: player4u and DoH cloudflare

---
+++ src/extractors/player4u.rs
@@ -0,0 +1,94 @@
+use std::sync::OnceLock;
+
+use regex::Regex;
+use scraper::Selector;
+
+use crate::{extractors::streamwish, models::ContentMediaItemSource, utils};
+
+#[derive(Debug)]
+struct Item {
+    url: String,
+    title: String,
+}
+
+pub async fn extract(
+    url: &str,
+    referer: &str,
+    prefix: &str,
+) -> anyhow::Result<Vec<ContentMediaItemSource>> {
+    let client = utils::create_client();
+
+    let res = client
+        .get(url)
+        .header("Refere", referer)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let items = lookup_items(&res, prefix);
+
+    // println!("{items:#?}");
+
+    let sub_extractors = items.iter().map(|item| {
+        let url = &item.url;
+        let prefix = &item.title;
+
+        streamwish::extract(url, referer, prefix)
+    });
+
+    let sources: Vec<_> = futures::future::join_all(sub_extractors)
+        .await
+        .into_iter()
+        .flatten()
+        .flatten()
+        .collect();
+
+    Ok(sources)
+}
+
+fn lookup_items(res: &str, prefix: &str) -> Vec<Item> {
+    let document = scraper::Html::parse_document(res);
+
+    let sel = Selector::parse("li.slide-toggle a.playbtnx").unwrap();
+
+    static ID_RE: OnceLock<Regex> = OnceLock::new();
+    let id_re = ID_RE.get_or_init(|| Regex::new(r"id=(?<id>[\w\d]+)").unwrap());
+
+    let player_url = streamwish::PLAYER_URL;
+    let items: Vec<_> = document
+        .select(&sel)
+        .filter_map(|el| {
+            let onclick = el.attr("onclick")?;
+            let title = el.text().collect::<Vec<_>>().join("");
+
+            let id = id_re
+                .captures(onclick)
+                .and_then(|c| c.name("id"))
+                .map(|m| m.as_str())?;
+
+            Some(Item {
+                url: format!("{player_url}/e/{id}"),
+                title: format!("[{prefix}] {title}"),
+            })
+        })
+        .collect();
+    items
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn should_extract() {
+        let res = extract(
+            "https://player4u.xyz/embed?key=The Flash s01e01",
+            "https://player4u.xyz",
+            "prefix",
+        )
+        .await;
+
+        println!("{res:#?}")
+    }
+}
---
+++ src/utils/doh.rs
@@ -0,0 +1,48 @@
+use hickory_resolver::config::ResolverConfig;
+use hickory_resolver::name_server::TokioConnectionProvider;
+use hickory_resolver::Resolver;
+use hickory_resolver::TokioResolver;
+use reqwest::dns::Addrs;
+use reqwest::dns::Name;
+use reqwest::dns::Resolve;
+use reqwest::dns::Resolving;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::sync::OnceLock;
+
+#[derive(Debug, Default, Clone)]
+pub struct DoHResolver {
+    state: Arc<OnceLock<TokioResolver>>,
+}
+
+impl DoHResolver {
+    fn init_resolver(&self) -> TokioResolver {
+        Resolver::builder(TokioConnectionProvider::default())
+            .unwrap_or_else(|_| {
+                Resolver::builder_with_config(
+                    ResolverConfig::cloudflare(),
+                    TokioConnectionProvider::default(),
+                )
+            })
+            .build()
+    }
+}
+
+impl Resolve for DoHResolver {
+    fn resolve(&self, name: Name) -> Resolving {
+        let hickory_resolver = self.clone();
+
+        Box::pin(async move {
+            let resolver = hickory_resolver
+                .state
+                .get_or_init(|| hickory_resolver.init_resolver());
+
+            let lookup = resolver.lookup_ip(name.as_str()).await?;
+
+            let addrs: Addrs = Box::new(lookup.into_iter().map(|addr| SocketAddr::new(addr, 0)));
+
+            Ok(addrs)
+        })
+    }
+}
+
